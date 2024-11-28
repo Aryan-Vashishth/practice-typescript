2e84529a3394e03cd891dba8da7fa8d6ff14219d
AzureAD\AryanVashishth <Aryan@vartopia.com>
2024-11-28 14:32:58 +0530

    Practice till "Enums"

--- TypeScript/Demo/app.js
+++ TypeScript/Demo/app.js
@@ -29,16 +29,55 @@ console.log("result2 = " + result2);
 var arr0 = [1, 2, 3, "hello"]; // "arr0" is name of array
 console.log(arr0);
 // arr0[0] + 1; //This will give an error because the arr in not exclusive for int here
-var arr = [1, 2, 3]; //defined datatype of array "arr" to int
+var arr; //defined datatype of array "arr" to int
+arr = [1, 2, 3];
 console.log(arr);
-var nestArr = [["hello", 1], [2, 3], ["hello", "world"], "hello world"]; //Nested array: array inside array
+var nestArr = [["hello", 1, 3], [2, 3], ["hello", "world"], "hello world"]; //Nested array: array inside array
 console.log(nestArr);
 console.log("Length: " + nestArr[3].length); //length of index 3: "hello world" i.e. 11
-//---------------------Tuple-----------------------------------------------------(fixed length array)
+//---------------------Tuple-----------------------------------------------------(fixed length array)-----------------
 const coord = [10, 20]; //tuple with 2 elements
 console.log("coord: " + coord[1]);
-const coords = [[-21, 20], [60, 20], [50, -100]];
-console.log(coords);
-console.log(coords[1]);
-console.log(coords[1][0]);
+const coords = [[-21, 20], [60, 20], [50, -100]]; //Tuples inside array
+console.log(coords); //print all tuples
+console.log(coords[1]); //print 2nd tuple
+console.log(coords[1][0]); //print 1st element on 2nd tuple
+//-----------------------Literals--------  ---------------(textual repesentation (notation) of a value as it is written in the source code)--------------
+let direction; // We can only assign thses literals ("north" , "south" , "east" , "west") to the variable "direction"
+direction = "north";
+// direction = "hello"; // (hello is not assignable to type '"north" | "south" | "east" | "west"') hence will throw an error
+console.log(direction);
+let responseCode;
+responseCode = 404;
+console.log(responseCode);
+let bool1;
+bool1 = true;
+console.log(bool1);
+//-------------------------------Enums--------------------------------------- "Enumeration" (enables developers to establish a collection of named constants (enumerators), each linked with an integer value)-----------------------
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 100] = "Small";
+    Size[Size["Medium"] = 500] = "Medium";
+    Size[Size["Large"] = 1000] = "Large";
+})(Size || (Size = {}));
+var companySize = Size.Large; // Assigning the `Large` enum member of `Size` to `companySize` (type-safe and descriptive).
+var companySize1 = 500; // Assigning the numeric value `500`, which corresponds to `Size.Large`, directly to `companySize1` (less descriptive and potentially error-prone).
+console.log(companySize);
+console.log(companySize1);
+if (companySize == Size.Large) {
+    console.log("The company size is 'Large' and the number of employs are " + "'" + companySize + "+'");
+}
+else if (companySize == Size.Small) {
+    console.log("The company size is 'Small' and the number of employs are " + "'" + companySize + "+'");
+}
+else {
+    console.log("The company size is 'Medium' and the number of employs are " + "'" + companySize + "+'");
+}
+var Validation;
+(function (Validation) {
+    Validation["emailField"] = "'@' symbol should be present";
+    Validation["passwordFieldBasic"] = "At least 8 characters is required";
+    Validation["passwordFieldAdvanced"] = "Password must meet the following requirements:\n    - At least 8 characters in length.\n- Contains at least one uppercase letter(A\uFFFDZ).\n- Contains at least one lowercase letter(a\uFFFDz).\n- Contains at least one number(0\uFFFD9).\n- Contains at least one special character(!@#$ %^&*).\n- Does not contain spaces.";
+})(Validation || (Validation = {}));
+console.log(Validation.passwordFieldAdvanced);
 //# sourceMappingURL=app.js.map
--- TypeScript/Demo/app.ts
+++ TypeScript/Demo/app.ts
@@ -43,7 +43,8 @@ var arr0 = [1, 2, 3, "hello"]; // "arr0" is name of array
 console.log(arr0);
 // arr0[0] + 1; //This will give an error because the arr in not exclusive for int here
 
-var arr: number[] = [1, 2, 3]; //defined datatype of array "arr" to int
+var arr: number[]; //defined datatype of array "arr" to int
+arr    = [1, 2, 3]; 
 console.log(arr);
 
 var nestArr = [["hello", 1, 3], [2, 3], ["hello", "world"], "hello world"]; //Nested array: array inside array
@@ -51,7 +52,9 @@ console.log(nestArr);
 console.log("Length: " + nestArr[3].length); //length of index 3: "hello world" i.e. 11
 
 
-//---------------------Tuple-----------------------------------------------------(fixed length array)
+
+
+//---------------------Tuple-----------------------------------------------------(fixed length array)-----------------
 
 const coord: [number, number] = [10, 20]; //tuple with 2 elements
 console.log("coord: " + coord[1]);
@@ -60,3 +63,74 @@ const coords: [number, number][] = [[-21, 20], [60, 20], [50, -100]]; //Tuples i
 console.log(coords); //print all tuples
 console.log(coords[1]); //print 2nd tuple
 console.log(coords[1][0]); //print 1st element on 2nd tuple
+
+
+
+
+
+//-----------------------Literals--------  ---------------(textual repesentation (notation) of a value as it is written in the source code)--------------
+
+let direction: "north" | "south" | "east" | "west"; // We can only assign thses literals ("north" , "south" , "east" , "west") to the variable "direction"
+direction = "north";
+// direction = "hello"; // (hello is not assignable to type '"north" | "south" | "east" | "west"') hence will throw an error
+console.log(direction);
+
+let responseCode: 200 | 404 | 201;
+responseCode = 404;
+console.log(responseCode);
+
+let bool1: true;
+bool1 = true;
+console.log(bool1);
+
+
+
+
+
+//-------------------------------Enums--------------------------------------- "Enumeration" (enables developers to establish a collection of named constants (enumerators), each linked with an integer value)-----------------------
+
+enum Size {
+    Small = 100,
+    Medium = 500,
+    Large = 1000
+}
+
+var companySize: Size = Size.Large; // Assigning the `Large` enum member of `Size` to `companySize` (type-safe and descriptive).
+var companySize1: Size = 500; // Assigning the numeric value `500`, which corresponds to `Size.Large`, directly to `companySize1` (less descriptive and potentially error-prone).
+
+console.log(companySize);
+console.log(companySize1);
+
+
+if (companySize == Size.Large) {
+    console.log("The company size is 'Large' and the number of employs are " + "'" + companySize + "+'");
+} else if (companySize == Size.Small) {
+    console.log("The company size is 'Small' and the number of employs are " + "'" + companySize + "+'");
+}
+else {
+    console.log("The company size is 'Medium' and the number of employs are " + "'" + companySize + "+'");
+}
+
+
+
+
+enum Validation {
+    emailField = "'@' symbol should be present",
+
+    passwordFieldBasic = "At least 8 characters is required",
+
+    passwordFieldAdvanced = `Password must meet the following requirements:
+    - At least 8 characters in length.
+    - Contains at least one uppercase letter(A–Z).
+    - Contains at least one lowercase letter(a–z).
+    - Contains at least one number(0–9).
+    - Contains at least one special character(!@#$ %^&*).
+    - Does not contain spaces.`
+}
+
+console.log(Validation.passwordFieldAdvanced);
+
+
+
+
+
